bff2798ebfa76f9c8c669a26fd2ffd99e19ae68f
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-12-24 22:26:54 +0700

    test token length, first and last tokens

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,45 +2,35 @@ import unittest
 import sys
 sys.path.append("../")
 from src.dataset import *
-
-en = [
-    'If you &apos;re a doctor you can do some good things, but if you &apos;re a caring doctor you can do some other things.',
-    'sHe &apos;d squEEze me',
-    'If I said , &quot; No , &quot; she &apos;d assault me again',
-    'I said , &quot; Okay , Mama . &quot;'
-]
-expected_en = [
-]
-
-vi = [
-    'Mẹ tôi hỏi nhà sư , &quot; Tại sao tay mẹ tôi ấm thế mà cả người lại lạnh ngắt ? &quot;',
-    '&quot; Các chị kHông hề buông tay. &quot;',
-    'Giờ đây tôi cũng muốn có con , và tôi băn khoăn về con thuyền .'
-]
-expected_vi = [
-
-]
-TARGET_LEN = 10
+from torchnlp.encoders.text import DEFAULT_SOS_INDEX
 
 class TestDataPreprocessor(unittest.TestCase):
     def test_pad(self):
         tokenized_sentence = torch.tensor([3, 1, 57, 328, 27, 481, 57, 2]) # 8
-        padded_sen = pad_or_truncate(tokenized_sentence, target_len=TARGET_LEN)
+        padded_sen = pad_or_truncate(tokenized_sentence, target_len=10)
         self.assertListEqual(padded_sen.tolist(), [3, 1, 57, 328, 27, 481, 57, 0, 0, 2])
 
     def test_truncate(self):
         tokenized_sentence = torch.tensor([3, 1, 57, 328, 27, 481, 57, 345, 11, 6, 189, 76, 482, 465, 2])  # 15
-        trucated_sen = pad_or_truncate(tokenized_sentence, target_len=TARGET_LEN)
+        trucated_sen = pad_or_truncate(tokenized_sentence, target_len=10)
         self.assertListEqual(trucated_sen.tolist(), [3, 1, 57, 328, 27, 481, 57, 345, 11, 2])
 
-    def test_apos_quot(self):
-        pass
-    def test_lower(self):
-        pass
-    def test_tokenize_en(self):
-        pass
-    def test_tokenize_vi(self):
-        pass
+    def test_data(self):
+        training_set = EnViTrainingDataSet(en_path="../data/train-en-vi/train.en",
+                                           vi_path="../data/train-en-vi/train.vi")
+
+        for i in range(10):
+            en_token = training_set[i][0].tolist()
+            vi_token = training_set[i][1].tolist()
+
+            self.assertEqual(len(en_token), TARGET_VALID_LEN+2)
+            self.assertEqual(len(vi_token), TARGET_VALID_LEN + 2)
+
+            self.assertEqual(en_token[0], DEFAULT_SOS_INDEX)
+            self.assertEqual(en_token[-1], DEFAULT_EOS_INDEX)
+
+            self.assertEqual(vi_token[0], DEFAULT_SOS_INDEX)
+            self.assertEqual(vi_token[-1], DEFAULT_EOS_INDEX)
 
 if __name__ == '__main__':
     unittest.main()
