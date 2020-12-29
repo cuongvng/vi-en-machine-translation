99b25a0ddf38d0926607070cea214bde7fc234ea
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2020-12-29 22:10:20 +0700

    mv checkpoint_dir to command line

--- src/train.py
+++ src/train.py
@@ -1,8 +1,8 @@
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
-from torchsummary import summary
 import os
+import argparse
 from pathlib import Path
 from dataset import IWSLT15EnViDataSet
 from model import NMT
@@ -14,17 +14,13 @@ from CONFIG import N_EPOCHS, BATCH_SIZE, EN2VI, MODE, MAX_LENGTH
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device: {device}")
 
-CHECKPOINT_DIR = "../model/"
-if not Path(CHECKPOINT_DIR).exists():
-    os.mkdir(CHECKPOINT_DIR)
-CHECKPOINT_PATH = Path(CHECKPOINT_DIR + "model.pt")
-
-def train():
+def train(checkpoint_path):
     # Data
     data_train = IWSLT15EnViDataSet(en_path="../data/train-en-vi/train.en",
                                     vi_path="../data/train-en-vi/train.vi")
     data_loader = DataLoader(data_train, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
     tgt_vocab_size = data_train.vi_vocab_size if MODE == EN2VI else data_train.en_vocab_size
+    print("Loading data done!")
 
     # Model & Optimizer
     model = NMT(mode=MODE, tgt_vocab_size=tgt_vocab_size)
@@ -34,12 +30,10 @@ def train():
     optimizer = Adam(model.parameters())
 
     prev_epochs = 0
-    if CHECKPOINT_PATH.exists():  # Resume training
-        model, optimizer, prev_epoch = load_checkpoint(model, optimizer)
+    if checkpoint_path.exists():  # Resume training
+        model, optimizer, prev_epoch = load_checkpoint(model, optimizer, checkpoint_path)
     else:
         model.apply(xavier_init_weights)
-
-    summary(model, input_size=MAX_LENGTH, batch_size=BATCH_SIZE, device=str(device))
 
     model.train()
     for epoch in range(N_EPOCHS-prev_epochs):
@@ -66,18 +60,19 @@ def train():
             del src, tgt, logit_outputs, loss
             torch.cuda.empty_cache()
 
-        save_checkpoint(model, optimizer, prev_epoch+epoch+1)
+        save_checkpoint(model, optimizer, prev_epoch+epoch+1, checkpoint_path)
 
-def save_checkpoint(model, optimizer, epoch):
+def save_checkpoint(model, optimizer, epoch, checkpoint_path):
     checkpoint = {
         "model": model.state_dict(),
         "optimizer": optimizer.state_dict(),
         "epoch": epoch,
     }
-    torch.save(checkpoint, CHECKPOINT_PATH)
+    torch.save(checkpoint, checkpoint_path)
+    print("Checkpoint saved!")
 
-def load_checkpoint(model, optimizer):
-    checkpoint = torch.load(CHECKPOINT_PATH)
+def load_checkpoint(model, optimizer, checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint["model"])
     optimizer.load_state_dict(checkpoint["optimizer"])
     prev_epoch = checkpoint["epoch"]
@@ -90,4 +85,13 @@ def xavier_init_weights(model):
     return
 
 if __name__ == "__main__":
-    train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("checkpoint_dir", type=str, default="../model/",
+                        help="Directory to save checkpoints")
+    args = parser.parse_args()
+    checkpoint_dir = args.checkpoint_dir
+    if not Path(checkpoint_dir).exists():
+        os.mkdir(checkpoint_dir)
+    CHECKPOINT_PATH = Path(os.path.join(checkpoint_dir, "model.pt"))
+
+    train(checkpoint_path=CHECKPOINT_PATH)
